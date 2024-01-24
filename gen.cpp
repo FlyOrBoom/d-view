42b43e434fdaa0a5ed06985266b86784e4bc7cc2
Xing Liu <31298876+FlyOrBoom@users.noreply.github.com>
2024-01-24 14:33:16 -0800

    Move sort to c++

--- gen.cpp
+++ gen.cpp
@@ -3,22 +3,55 @@
 #include <fstream>
 #include <iostream>
 #include <stdexcept>
+#include <utility>
+#include <map>
 
 namespace fs = std::filesystem;
 
-void hierarchy(fs::path path, int depth)
+enum Entry_type { 
+    directory = 0,
+    file = 1
+};
+
+struct Entry_key {
+    std::string entry_name;
+    Entry_type entry_type;
+
+    std::strong_ordering operator<=>(const Entry_key& other) const {
+        auto compare_name = entry_name <=> other.entry_name; 
+        auto compare_type = entry_type <=> other.entry_type; 
+        if (compare_type != 0) return compare_type;
+        return compare_name;
+    }
+};
+
+
+void hierarchy(fs::path path, struct Entry_key key, int depth)
 {
     std::cout << "[";
-    std::cout << path.filename();
+    std::cout << "\"" << key.entry_name << "\"";
 
-    if(fs::is_directory(path)) {
-        if(depth == 0) {
-            std::cout << ",0";
-        } else {
-            for(const fs::directory_entry& dir_entry : fs::directory_iterator{path})
+    if(key.entry_type == directory) {
+        std::cout << ",0";
+
+        if(depth != 0) 
+        {
+            std::map<struct Entry_key, fs::path> sorted_dir;
+
+            for(const auto& dir_entry : fs::directory_iterator{path})
+            {
+                fs::path entry_path = dir_entry.path();
+                struct Entry_key entry_key = {
+                    .entry_name = entry_path.filename(),
+                    .entry_type = (dir_entry.is_directory()) ? directory : file,
+                };
+
+                sorted_dir[ entry_key ] = entry_path;
+            }
+            for(const auto&[ entry_key, entry_path ] : sorted_dir)
             {
                 std::cout << ",";
-                hierarchy(dir_entry, depth - 1);
+                hierarchy(entry_path, entry_key, depth - 1);
             }
         }
     } else {
@@ -41,7 +74,11 @@ int main(int argc, char* argv[])
     int max_depth = (argc >= 3) ? atoi(argv[2]) : -1;
 
     const fs::path base_path{base_directory};
+    const struct Entry_key base_key = { 
+        .entry_name = base_path.filename(),
+        .entry_type = directory,
+    };
 
-    hierarchy(base_path, max_depth);
+    hierarchy(base_path, base_key, max_depth);
     std::cout << "\n";
 }
