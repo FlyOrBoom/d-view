d1aa63625f4bb768dbec50463721e1e919c5be17
Xing Liu <31298876+FlyOrBoom@users.noreply.github.com>
2024-01-24 01:13:09 -0800

    Revert to not parallelized, as the nersc g++ compiler appear to not support parallelized for_each?

--- gen-par.cpp
+++ gen-par.cpp
@@ -2,17 +2,28 @@
 #include <filesystem>
 #include <fstream>
 #include <iostream>
-
+#include <vector>
+#include <execution>
+ 
 namespace fs = std::filesystem;
 
 void hierarchy(fs::path path)
 {
     std::cout << "[";
     std::cout << path.filename();
-
+    
     if(fs::is_directory(path)) {
+        std::vector<fs::directory_entry> it;
         std::ranges::for_each(
             fs::directory_iterator{path},
+            [&](const auto& dir_entry){
+                it.push_back(dir_entry);
+            }
+        );
+        std::for_each(
+            std::execution::par,
+            std::begin(it),
+            std::end(it),
             [](const auto& dir_entry){
                 std::cout << ",";
                 hierarchy(dir_entry);
--- test.sh
+++ test.sh
@@ -1,1 +1,2 @@
-time ( repeat 100 { python3 gen.py "../.somewhere" > dir_structure.py } ) && time ( repeat 100 { ./gen ../.somewhere > dir_structure.py } )
+time ( repeat 100 { ./gen ../.somewhere > dir_structure.py } )
+time ( repeat 100 { ./gen-new ../.somewhere > dir_structure.py } )
